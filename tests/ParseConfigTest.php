1aa7cb726505a5905ba6376ad3dbee35e9116fb6
Fosco Marotto <fjm@fb.com>
2014-10-22 16:31:57 -0700

    Added ParseConfig

---
+++ tests/ParseConfigTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use Parse\ParseConfig;
+
+require_once 'ParseTestHelper.php';
+
+class ParseConfigMock extends ParseConfig {
+  public function __construct() {
+    $this->setConfig(["foo" => "bar", "some" => 1]);
+  }
+}
+
+class ParseConfigTest extends PHPUnit_Framework_TestCase
+{
+
+  public function testGetConfig()
+  {
+    $config = new ParseConfigMock();
+    $this->assertEquals("bar", $config->get("foo"));
+    $this->assertEquals(1, $config->get("some"));
+  }
+
+}
