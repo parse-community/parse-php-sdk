3ca3e9751f77c0746bc35a4fbcbb34125295676f
Fosco Marotto <fjm@fb.com>
2015-03-25 10:34:18 -0700

    Updated to 1.1.0

--- src/Parse/ParseClient.php
+++ src/Parse/ParseClient.php
@@ -53,7 +53,7 @@ final class ParseClient
    * Constant for version string to include with requests.
    * @ignore
    */
-  const VERSION_STRING = 'php1.0.6';
+  const VERSION_STRING = 'php1.1.0';
 
   /**
    * Parse\Client::initialize, must be called before using Parse features.
