3d279ad951610cd1197070b0e0e1a9ed78b888a0
Fosco Marotto <gfosco@gmail.com>
2015-11-03 14:06:59 -0800

    Updating to 1.1.8

--- src/Parse/ParseClient.php
+++ src/Parse/ParseClient.php
@@ -83,7 +83,7 @@ final class ParseClient
      *
      * @var string
      */
-    const VERSION_STRING = 'php1.1.7';
+    const VERSION_STRING = 'php1.1.8';
 
     /**
      * Parse\Client::initialize, must be called before using Parse features.
