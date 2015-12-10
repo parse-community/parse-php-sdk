6623fda079a22dc6bbd5a17eeab70995f8b38f50
mahavirnahata <mahavirnahata@gmail.com>
2015-12-10 18:13:01 +0530

    Sql like %value% patern matching case insensitive

--- src/Parse/ParseQuery.php
+++ src/Parse/ParseQuery.php
@@ -878,7 +878,9 @@ class ParseQuery
      * works like same as SQL LIKE %value% syntax which is case insensitive.
      *
      * @param string $key   The key to check.
-     * @param mixed  $value The substring to match with key , its case insensitive operation remove (?i) from below regex to make it case sensitive.
+     * @param mixed  $value The substring to match with key , 
+     * its case insensitive operation 
+     * remove (?i) from below regex to make it case sensitive.
      *
      * @return ParseQuery Returns this query, so you can chain this call.
      */
