fcc9746da97284fed3cbf13364a900befcf6a1e6
Benjamin <friedman.benjamin@gmail.com>
2016-03-04 20:38:47 -0800

    Removed appended version number causing batch requests to fail

--- src/Parse/ParseObject.php
+++ src/Parse/ParseObject.php
@@ -1039,7 +1039,7 @@ class ParseObject implements Encodable
                 $batch[0]->mergeAfterSave($result);
             } else {
                 foreach ($requests as &$r) {
-                    $r['path'] = '/1/'.$r['path'];
+                    $r['path'] = $r['path'];
                 }
                 $result = ParseClient::_request(
                     'POST',
