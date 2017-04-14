7462e3039e9f2cd73bd9ed81691888a743ee316a
Benjamin <Bfriedman@lavia.local>
2017-04-14 11:18:22 -0700

    Added property getters to ParseInstallation

--- src/Parse/ParseInstallation.php
+++ src/Parse/ParseInstallation.php
@@ -10,4 +10,148 @@ namespace Parse;
 class ParseInstallation extends ParseObject
 {
     public static $parseClassName = '_Installation';
+
+    /**
+     * Gets the installation id for this installation
+     *
+     * @return string
+     */
+    public function getInstallationId()
+    {
+        return $this->get('installationId');
+
+    }
+
+    /**
+     * Gets the device token for this installation
+     *
+     * @return string
+     */
+    public function getDeviceToken()
+    {
+        return $this->get('deviceToken');
+
+    }
+
+    /**
+     * Get channels for this installation
+     *
+     * @return array
+     */
+    public function getChannels()
+    {
+        return $this->get('channels');
+
+    }
+
+    /**
+     * Gets the device type of this installation
+     *
+     * @return string
+     */
+    public function getDeviceType()
+    {
+        return $this->get('deviceType');
+
+    }
+
+    /**
+     * Gets the push type of this installation
+     *
+     * @return string
+     */
+    public function getPushType()
+    {
+        return $this->get('pushType');
+
+    }
+
+    /**
+     * Gets the GCM sender id of this installation
+     *
+     * @return string
+     */
+    public function getGCMSenderId()
+    {
+        return $this->get('GCMSenderId');
+
+    }
+
+    /**
+     * Gets the time zone of this installation
+     *
+     * @return string
+     */
+    public function getTimeZone()
+    {
+        return $this->get('timeZone');
+
+    }
+
+    /**
+     * Gets the locale id for this installation
+     *
+     * @return string
+     */
+    public function getLocaleIdentifier()
+    {
+        return $this->get('localeIdentifier');
+
+    }
+
+    /**
+     * Gets the badge number of this installation
+     *
+     * @return int
+     */
+    public function getBadge()
+    {
+        return $this->get('badge');
+
+    }
+
+    /**
+     * Gets the app version of this installation
+     *
+     * @return string
+     */
+    public function getAppVersion()
+    {
+        return $this->get('appVersion');
+
+    }
+
+    /**
+     * Get the app name for this installation
+     *
+     * @return string
+     */
+    public function getAppName()
+    {
+        return $this->get('appName');
+
+    }
+
+    /**
+     * Gets the app identifier for this installation
+     *
+     * @return string
+     */
+    public function getAppIdentifier()
+    {
+        return $this->get('appIdentifier');
+
+    }
+
+    /**
+     * Gets the parse version for this installation
+     *
+     * @return string
+     */
+    public function getParseVersion()
+    {
+        return $this->get('parseVersion');
+
+    }
+
 }
