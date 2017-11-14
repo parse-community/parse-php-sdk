af27c70374eaa812a45ea78a7274dd220e754a1b
Benjamin Wilson Friedman <friedman.benjamin@gmail.com>
2017-11-13 22:02:42 -0800

    Adds jobs to ParseCloud (#373)
    * Adds jobs to ParseCloud
    
    * Updates parse-server-test dependency to 1.3.4

--- tests/Parse/ParseCloudTest.php
+++ tests/Parse/ParseCloudTest.php
@@ -109,4 +109,76 @@ class ParseCloudTest extends \PHPUnit_Framework_TestCase
         );
         ParseCloud::run('unknown_function', $params);
     }
+
+    /**
+     * @group cloud-code-jobs
+     */
+    public function testGetJobsData()
+    {
+        $jobsData = ParseCloud::getJobsData();
+        $this->assertNotNull($jobsData['jobs']);
+        $this->assertNotNull($jobsData['in_use']);
+        $this->assertEquals(0, count($jobsData['in_use']));
+        $this->assertEquals(3, count($jobsData['jobs']));
+    }
+
+    /**
+     * @group cloud-code-jobs
+     */
+    public function testRunJob()
+    {
+        $jobStatusId = ParseCloud::startJob('CloudJob1', [
+            'startedBy' => 'Monty Python'
+        ]);
+        $this->assertNotNull($jobStatusId);
+
+        $jobStatus = ParseCloud::getJobStatus($jobStatusId);
+        $this->assertNotNull($jobStatus);
+        $this->assertEquals('succeeded', $jobStatus->get('status'));
+        $this->assertEquals('Monty Python', $jobStatus->get('params')['startedBy']);
+    }
+
+    /**
+     * @group cloud-code-jobs
+     */
+    public function testLongJob()
+    {
+        $jobStatusId = ParseCloud::startJob('CloudJob2');
+        $jobStatus = ParseCloud::getJobStatus($jobStatusId);
+        $this->assertNotNull($jobStatus);
+        $this->assertEquals('running', $jobStatus->get('status'));
+    }
+
+    /**
+     * @group cloud-code-jobs
+     */
+    public function testBadJob()
+    {
+        $this->setExpectedException('Parse\ParseException', 'Invalid job.');
+        ParseCloud::startJob('bad_job');
+    }
+
+    /**
+     * @group cloud-code-jobs
+     */
+    public function testFailingJob()
+    {
+        $jobStatusId = ParseCloud::startJob('CloudJobFailing');
+        $this->assertNotNull($jobStatusId);
+
+        $jobStatus = ParseCloud::getJobStatus($jobStatusId);
+        $this->assertNotNull($jobStatus);
+        $this->assertEquals('failed', $jobStatus->get('status'));
+        $this->assertEquals('cloud job failed', $jobStatus->get('message'));
+    }
+
+    /**
+     * @group cloud-code-jobs
+     */
+    public function testGettingNotARealJobStatus()
+    {
+        $this->setExpectedException('Parse\ParseException', 'Object not found.');
+        $jobStatus = ParseCloud::getJobStatus('not-a-real-job-status');
+        $this->assertNull($jobStatus);
+    }
 }
