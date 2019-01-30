d7e42c9e4bdf4f534dd848d02d1f8ab62b25d40e
Arthur Cinader <700572+acinader@users.noreply.github.com>
2019-01-30 13:01:45 -0800

    prep for release 1.5.1

--- src/Parse/ParseClient.php
+++ src/Parse/ParseClient.php
@@ -111,11 +111,11 @@ final class ParseClient
     private static $caFile;
 
     /**
-     * Constant for version string to include with requests. Currently 1.5.0.
+     * Constant for version string to include with requests. Currently 1.5.1.
      *
      * @var string
      */
-    const VERSION_STRING = 'php1.5.0';
+    const VERSION_STRING = 'php1.5.1';
 
     /**
      * Parse\Client::initialize, must be called before using Parse features.
